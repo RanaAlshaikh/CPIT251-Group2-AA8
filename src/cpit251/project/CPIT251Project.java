5cd214685f88cc351786aa11d5b25a586c6a53ff
Rana Alshaikh <rmubarakalshaikh@stu.kau.edu.sa>
2023-11-12 16:34:57 +0300

    Rana commit ...

---
+++ src/cpit251/project/CPIT251Project.java
@@ -0,0 +1,14 @@
+
+package cpit251.project;
+
+/**
+ *
+ * @author RANA
+ */
+public class CPIT251Project {
+
+    public static void main(String[] args) {
+        System.out.println("CPIT251 Project");
+    }
+    
+}
